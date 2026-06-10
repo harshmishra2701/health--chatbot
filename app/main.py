ae69989179cbcc0a143d600dda3fe7bd841bc349
harshmishra2701 <mishraharsh649@gmail.com>
2026-06-10 13:13:54 +0530

    implement chatbot functionality with user session management and basic health inquiries; add HTML and CSS for UI layout

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+
 from app.routers.chat import router
 
 import uvicorn
